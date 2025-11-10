001f202ed5b6f2f6685bd428d74d9e5825e4a808
Ketlyn Lorayne Niza de Araujo <51931882819@>
2025-11-10 15:02:22 -0300

    Editing loginpage - 10/11/2025

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -4,9 +4,8 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
   const [login, setLogin] = useState('');
-  const [password, setPassword] = useState('')
-  const loginBg = new URL('../../assets/images/login.svg', import.meta.url).href;
-
+  const [password, setPassword] = useState('');
+  
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("Tentativa de login com: ", { login, password });
@@ -15,49 +14,65 @@ const Login = () => {
   return (
     <div className='login-page'>
       <div className='login-container'>
-        <span className='logo-let'>Let</span>
-        <span className='logo-movie'>Movie</span>
+        
+        {/* Logo */}
+        <div className="login-logo-container">
+          <span className='logo-let'>Let</span>
+          <span className='logo-movie'>Movie</span>
+        </div>
 
+        {/* Conteúdo principal */}
         <div className='login-content'>
+          
+          {/* Imagem */}
           <div className='login-image'>
-            <img src={new URL('../../assets/images/popcorn.svg', import.meta.url).href} alt="Pipoca e claquete" />
+            <img 
+              src={new URL('../../assets/images/popcorn.svg', import.meta.url).href} 
+              alt="Pipoca e claquete" 
+            />
+          </div>
 
+          {/* Formulário */}
+          <div className='login-form-content'>
+            <h2>Login</h2>
+            
+            <form onSubmit={handleSubmit} className='login-form'>
+              <label htmlFor="login-input">Login:</label>
+              <input 
+                type='text'
+                id='login-input'
+                placeholder='Digite seu email ou nome de usuário'
+                value={login}
+                onChange={(e) => setLogin(e.target.value)}
+                required
+              />
 
+              <label htmlFor="password-input">Senha:</label>
+              <input 
+                type='password'
+                id='password-input'
+                placeholder='Digite sua senha'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
 
-            <div className='login-form-content'>
-              <h2>Login</h2>
-              <form onSubmit={handleSubmit} className='login-form'>
-                <label htmlFor="login-input">Login:</label>
-                <input type='text'
-                  id='login-input'
-                  placeholder='Digite seu email ou nome de usuário'
-                  value={login}
-                  onChange={(e) => setLogin(e.target.value)}
-                  required
-                />
+              <button type='submit' className='login-button'>
+                Entrar
+              </button>
+            </form>
 
-                <label htmlFor="login-input">Password:</label>
-                <input type='password'
-                  id='login-input'
-                  placeholder='Digite sua senha'
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-
-                <button type='submit' className='login-button'>
-                  Entrar
-                </button>
-              </form>
-
-              {/* Esqueci a senha */}
-              <div className='login-forget'>
-                <Link to="#" className='forget-password'>
-                  <p>
-                    Esqueceu a senha?
-                  </p>
-                </Link>
-              </div>
+            {/* Esqueci a senha */}
+            <div className='login-links'>
+              <Link to="#" className='forgot-password'>
+                Esqueceu a senha?
+              </Link>
+              <p className='cadastrar'>
+              <Link to="#" className='sign-up'>
+                <span className='signup-white'>Não possui uma conta?</span>
+                <span className='signup-underline'> Cadastre-se</span>
+              </Link>
+              </p>
             </div>
           </div>
         </div>
@@ -66,4 +81,4 @@ const Login = () => {
   );
 };
 
-export default Login; 
+export default Login;
